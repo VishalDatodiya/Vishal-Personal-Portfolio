062f2367fe156f1f7bd10cdcb483cc7ff8738b4c
vishalDatodiya <vishaldatodiya12@gmail.com>
2023-04-22 20:01:42 +0530

    Removerd vercel and build_files

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2e7vmejlvgw!bdk&y)gmo&l-_2e-=oep(1cz%dj63igqx+kn!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -126,9 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
